1681e0e5289adeaf7935d2918f1320a865bd335f
marwa jafail <marwa.a.jafail@gmail.com>
2024-11-12 21:11:01 +0300

    add Security

---
+++ src/main/java/com/example/chatApp/security/JWTUtils.java
@@ -0,0 +1,59 @@
+package com.example.chatApp.security;
+import io.jsonwebtoken.*;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+//Generation Token, Logger (log in messages), validation token
+@Service
+public class JWTUtils {
+
+    Logger logger = Logger.getLogger(JWTUtils.class.getName());
+
+    @Value("${jwt-secret}")
+    private String jwtSecret;
+
+    @Value("${jwt-expiration-ms}")
+    private int jwtExpirationMs;
+
+    //Generation of token
+    public String generateJwtToken(MyUserDetails myUserDetails) {
+        return Jwts.builder()
+                .setSubject((myUserDetails.getUsername()))
+                .setIssuedAt(new Date())
+                .setExpiration(new Date((new Date()).getTime() + jwtExpirationMs))
+                .signWith(SignatureAlgorithm.HS256, jwtSecret)
+                .compact();
+    }
+
+    public String getUsernameFromToken(String token) {
+        return Jwts.parserBuilder().setSigningKey(jwtSecret).build().parseClaimsJws(token).getBody().getSubject();
+
+    }
+
+
+    public boolean validateJwtToken(String authToken){
+        System.out.println(authToken);
+        try {
+            Jwts.parser().setSigningKey(jwtSecret).parseClaimsJws(authToken);
+            return true;
+        } catch(SecurityException e) {
+            logger.log(Level.SEVERE, "Invalid JWT signature: {}", e.getMessage());
+        } catch(MalformedJwtException e) {
+            logger.log(Level.SEVERE, "Invalid JWT token: {}", e.getMessage());
+        } catch (ExpiredJwtException e) {
+            logger.log(Level.SEVERE, "JWT token expired: {}", e.getMessage());
+        } catch (UnsupportedJwtException e) {
+            logger.log(Level.SEVERE, "JWT token is unsupported: {}", e.getMessage());
+        } catch (IllegalArgumentException e){
+            logger.log(Level.SEVERE, "JWT claims string is empty: {}", e.getMessage());
+        }
+
+        return false;
+    }
+
+
+}
---
+++ src/main/java/com/example/chatApp/security/MyUserDetailsService.java
@@ -0,0 +1,25 @@
+package com.example.chatApp.security;
+
+import com.example.chatApp.model.User;
+import com.example.chatApp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MyUserDetailsService implements UserDetailsService {
+    private UserService userService;
+
+    @Autowired
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userService.findUserWithEmail(email);
+        return new MyUserDetails(user);
+    }
+}
