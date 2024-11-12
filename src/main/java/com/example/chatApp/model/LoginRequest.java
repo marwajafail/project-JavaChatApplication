351e8ff41f20d485fde7d9dbbf675b4b5acfc6fe
marwa jafail <marwa.a.jafail@gmail.com>
2024-11-12 21:08:18 +0300

    add LoginResponse & LoginRequest model

---
+++ src/main/java/com/example/chatApp/model/LoginRequest.java
@@ -0,0 +1,11 @@
+package com.example.chatApp.model;
+
+import lombok.Getter;
+
+@Getter
+public class LoginRequest {
+
+    private String email;
+    private String password;
+
+}
---
+++ src/main/java/com/example/chatApp/model/LoginResponse.java
@@ -0,0 +1,13 @@
+package com.example.chatApp.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter @Setter
+public class LoginResponse {
+
+    private String message;
+
+}
